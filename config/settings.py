06b181e9764ce10a8a7d0ce7fe0b2364237df4e2
Thomas <thomasgodfrey448@gmail.com>
2026-03-20 03:35:44 +0300

    Add WhiteNoise to requirements

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-financial-dashboard-2024-tsh-professional'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
